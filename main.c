96528f0eda6c3114a06d0c75bbe387981dedd268
KHANH DO <bkdo30@gmail.com>
2018-04-03 10:39:32 +0000

    Fifth commit

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(void){
 	int ret;
 	while(1){	//loop forever
 		//run system command "arecord" to record 1 sec of wav
-		ret=system("arecord -r16000 -c1 -f S16_LE -d1 test.wav");
+		ret=system("arecord -q -r16000 -c1 -f S16_LE -d1 test.wav");
 		if(WIFSIGNALED(ret) && (WTERMSIG(ret) == SIGINT)) break;
 		//display wav header
 		dispWAVHeader("test.wav");
--- screen.h
+++ screen.h
@@ -1,3 +1,5 @@
+#define UNICODE		//use unicode to make good-looking bar
+#define BAR "\u2590"
 #define ESC 0x1B
 // constant definitions
 enum COLORS {BLACK = 30, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE};
